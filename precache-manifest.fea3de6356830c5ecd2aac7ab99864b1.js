9440c5fdd493566ceb6e2d9a1bbee03b9681a8a0
msaaddev <mrsaadirfan@gmail.com>
2020-06-26 16:42:46 +0500

    Updates

---
+++ precache-manifest.fea3de6356830c5ecd2aac7ab99864b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db35e7356c3cf435aee7e008029fa522",
+    "url": "/Where-Are-You-From/index.html"
+  },
+  {
+    "revision": "3709833bb3ba4c93f75d",
+    "url": "/Where-Are-You-From/static/css/main.6c6ca667.chunk.css"
+  },
+  {
+    "revision": "60389cb11a2a9ec1ca94",
+    "url": "/Where-Are-You-From/static/js/2.7ea77eb7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Where-Are-You-From/static/js/2.7ea77eb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3709833bb3ba4c93f75d",
+    "url": "/Where-Are-You-From/static/js/main.87545849.chunk.js"
+  },
+  {
+    "revision": "48fac1966abbd0408ef9",
+    "url": "/Where-Are-You-From/static/js/runtime-main.64d9d11a.js"
+  }
+]);
