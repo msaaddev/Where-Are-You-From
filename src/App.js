57210e3a9d20fc5754d06ff9cdc04dc7bbb7cb8d
msaaddev <mrsaadirfan@gmail.com>
2020-11-01 17:31:07 +0500

    📦 NEW: api

--- src/App.js
+++ src/App.js
@@ -1,30 +1,24 @@
-import React, { useState, useEffect } from "react";
-import axiox from "axios";
-import "./App.css";
+import React, { useState, useEffect } from 'react';
+import axiox from 'axios';
+import './App.css';
 
 function App() {
-  const [countryInfo, setCountryInfo] = useState("");
-  const [location, setLocation] = useState("");
+    const [countryInfo, setCountryInfo] = useState('');
 
-  useEffect(() => {
-    (async () => {
-      const { data } = await axiox.get(
-        "http://api.ipstack.com/check?access_key=YOUR_API_ACCESS_KEY",
-      );
-      setCountryInfo(data);
-      setLocation(data.location);
-    })();
-  }, []);
+    useEffect(() => {
+        (async () => {
+            const { data } = await axiox.get('http://ipwhois.app/json/');
+            setCountryInfo(data);
+        })();
+    }, []);
 
-  return (
-    <div className="container">
-      <div className="countryInfo">
-        <p>
-          {` You are living in ${countryInfo.country_name} ${location.country_flag_emoji}`}
-        </p>
-      </div>
-    </div>
-  );
+    return (
+        <div className='container'>
+            <div className='countryInfo'>
+                <h1>{` You are living in ${countryInfo.country} 😈`}</h1>
+            </div>
+        </div>
+    );
 }
 
 export default App;
